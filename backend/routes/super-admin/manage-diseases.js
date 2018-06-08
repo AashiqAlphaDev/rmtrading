a553db4a5366fe980e809c93af40696ce46cd712
Karthik Thirumalasetti <karthik@Karthiks-MacBook-Pro.local>
2018-06-08 20:33:58 +0530

    changes

--- backend/routes/super-admin/manage-diseases.js
+++ backend/routes/super-admin/manage-diseases.js
@@ -1,25 +1,35 @@
 var Router = require("express").Router
 var router = Router();
 const co = require("co");
+const DiseaseManagementService = require("../../services/diseases")
 
-router.get("/diseases", co.wrap(function*(req, res, next){
-
+router.get("/", co.wrap(function*(req, res, next){
+	var query = {}
+	if(req.query.q){
+		query.name = {$regex:`.*${req.query.q}.*`}
+	}
+	let diseases = yield DiseaseManagementService.diseases(req.query);
+	res.send(diseases);
 }));
 
-router.get("/diseases/:disease_id", co.wrap(function*(req, res, next){
-
+router.get("/:disease_id", co.wrap(function*(req, res, next){
+	let disease = yield DiseaseManagementService.diseaseWithId(req.params.disease_id);
+	res.send(disease);
 }));
 
-router.post("/diseases", co.wrap(function*(req, res, next){
-
+router.post("/", co.wrap(function*(req, res, next){
+	let disease = yield DiseaseManagementService.createDisease(req.body);
+	res.send(disease);
 }));
 
-router.put("/diseases/:disease_id", co.wrap(function*(req, res, next){
-
+router.put("/:disease_id", co.wrap(function*(req, res, next){
+	let disease = yield DiseaseManagementService.updateDisease(req.params.disease_id,req.body);
+	res.send(disease);
 }));
 
-router.delete("/diseases/:disease_id", co.wrap(function*(req, res, next){
-
+router.delete("/:disease_id", co.wrap(function*(req, res, next){
+	yield DiseaseManagementService.deleteDisease(req.params.disease_id);
+	res.send({});
 }));
 
 
--- frontend/store/super-admin/reducer.js
+++ frontend/store/super-admin/reducer.js
@@ -1,4 +1,10 @@
 import {SUPER_SIGN_OUT_REQUESTED, SUPER_SIGN_OUT_FAILED, SUPER_SIGN_OUT_SUCCEEDED} from "./auth-actions";
+import {
+	DISEASES_CLEAR_MATCHES,
+	DISEASES_FETCH_MATCHES,
+	DISEASES_FETCH_MATCHES_FAILED,
+	DISEASES_FETCH_MATCHES_SUCCEDED
+} from "./app-data-actions";
 import {combineReducers} from "redux";
 
 const initialAuthState = {
@@ -25,11 +31,33 @@ const authReducer = function (state = initialAuthState, action) {
 	return  state;
 }
 
-const initialAppData={}
+const initialDiseases = {
+	query:null,
+	matched_diseases:[],
+	fetch_matches_error:null
+};
 
-const appDataReducer = function(state=initialAppData, action){
+const diseaseListReducer = function(state=initialDiseases, action){
+	switch (action.type) {
+		case DISEASES_CLEAR_MATCHES:{
+			state = {...state, matched_diseases:[]};
+			return state;
+		}
+		case DISEASES_FETCH_MATCHES:{
+			state = {...state, fetch_matches_error:null, query:action.payload.query};
+			return state;
+		}
+		case DISEASES_FETCH_MATCHES_SUCCEDED:{
+			state = {...state, matched_diseases:action.payload};
+			return state;
+		}
+		case DISEASES_FETCH_MATCHES_FAILED:{
+			state = {...state, fetch_matches_error:action.payload};
+			return state;
+		}
+	}
 	return state;
 }
 
 
-export default combineReducers({authData: authReducer, appData:appDataReducer});
+export default combineReducers({authData: authReducer, diseaseList:diseaseListReducer});
