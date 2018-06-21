94f314d5383a130cd199ac8719bf4a19c441de6f
Aashiq <aashiqalimohammed@gmail.com>
2018-06-21 18:55:45 +0530

    changes

--- frontend-client/src/admin/dashboard/dashboard-layout.js
+++ frontend-client/src/admin/dashboard/dashboard-layout.js
@@ -11,105 +11,107 @@ import Tab from "@material-ui/core/es/Tab/Tab";
 import _ from "underscore";
 import {AccountIcon, AppsIcon, BellOutlineIcon} from "mdi-react";
 
-let Icon = (_Icon)=>{return (props)=>(<_Icon {...props} size={20} />)};
+let Icon = (_Icon) => {
+    return (props) => (<_Icon {...props} size={20}/>)
+};
 
 const pages = [
-	{icon: AppsIcon, label: "Overview", url: "/admin/dashboard"},
-	{icon: AppsIcon, label: "Pets", url: "/admin/dashboard/pets"},
-	{icon: AppsIcon, label: "Vaccination", url: "/admin/dashboard/vaccinations"},
-	{icon: AppsIcon, label: "Manage Appointments", url: "/admin/dashboard/inventory"},
-	{icon: AppsIcon, label: "Make Requests", url: "/admin/dashboard/requests"},
+    {icon: AppsIcon, label: "Overview", url: "/admin/dashboard"},
+    {icon: AppsIcon, label: "Pets", url: "/admin/dashboard/pets"},
+    {icon: AppsIcon, label: "Vaccination", url: "/admin/dashboard/vaccinations"},
+    {icon: AppsIcon, label: "Manage Appointments", url: "/admin/dashboard/inventory"},
+    {icon: AppsIcon, label: "Make Requests", url: "/admin/dashboard/requests"}
 ];
 
 let Index = withStyles((theme) => {
-	return (
-		{
-			...style(theme),
-			fullScreen: {
-				width: "100%",
-				height: "100%",
-				display: "flex",
-				flexDirection: "column"
-			},
-			selected: {
-				fill: theme.palette.secondary.main
-			},
-			navIcon: {
-				margin: theme.spacing.unit * 1
-			}
-		}
-	)
+    return (
+        {
+            ...style(theme),
+            fullScreen: {
+                width: "100%",
+                height: "100%",
+                display: "flex",
+                flexDirection: "column"
+            },
+            selected: {
+                fill: theme.palette.secondary.main
+            },
+            navIcon: {
+                margin: theme.spacing.unit * 1
+            }
+        }
+    )
 })(class extends React.Component {
-	state = {
-		anchorEl: null
-	}
+    state = {
+        anchorEl: null
+    }
 
-	componentWillMount() {
-		this.props.dispatch({type: CHECK_ADMIN})
-	}
+    componentWillMount() {
+        this.props.dispatch({type: CHECK_ADMIN})
+    }
 
-	render() {
-		const {classes} = this.props;
-		var currentTab = 0;
-		if (this.props.currentPage !== "") {
-			currentTab = _.findIndex(pages, (item) => {
-				return item.url === this.props.currentPage
-			});
-		}
-		return <div className={classes.fullScreen}>
-			<Layout direction={"column"} className={`flex`}>
-				<AppBar position="static" color="default">
-					<Toolbar className={`container`}>
-						<Layout direction={"column"} className={`flex`}>
-							<Layout alignItems={"center"}>
-								<div className={`flex`}>
-									<Link to={"/super-admin/dashboard/"} className={classes.logo}>
-										<img src={"/logo.png"} style={{height: 40}} alt={"logo"}/>
-									</Link>
-								</div>
-								<div>
-									<IconButton>
-										<BellOutlineIcon/>
-									</IconButton>
-									<IconButton onClick={(event) => {
-										this.setState({anchorEl: event.currentTarget})
-									}}>
-										<AccountIcon/>
-									</IconButton>
-									<Menu
-										anchorEl={this.state.anchorEl}
-										open={Boolean(this.state.anchorEl)}
-										onClose={() => {
-											this.setState({anchorEl: null})
-										}}
-									>
-										<MenuItem onClick={this.handleClose}>Logout</MenuItem>
-									</Menu>
-								</div>
-							</Layout>
-							<Layout direction="column">
-								<Tabs centered fullWidth value={currentTab}>
-									{
-										pages.map((page, index) => {
-											let PageIcon = Icon(page.icon);
-											let iconClass = (index == currentTab) ? classes.selected : '';
-											return <Tab classes={{label: classes.tabIcon}}
-											            label={<Layout alignItems={"center"}><PageIcon
-												            className={`${iconClass} ${this.props.classes.navIcon}`}/>{page.label}
-											            </Layout>}
-											            component={Link} to={page.url} key={index}>
-											</Tab>;
-										})
-									}
-								</Tabs>
-							</Layout>
-						</Layout>
-					</Toolbar>
-				</AppBar>
-				{this.props.children}
-			</Layout>
-		</div>;
-	}
+    render() {
+        const {classes} = this.props;
+        var currentTab = 0;
+        if (this.props.currentPage !== "") {
+            currentTab = _.findIndex(pages, (item) => {
+                return item.url === this.props.currentPage
+            });
+        }
+        return <div className={classes.fullScreen}>
+            <Layout direction={"column"} className={`flex`}>
+                <AppBar position="static" color="default">
+                    <Toolbar className={`container`}>
+                        <Layout direction={"column"} className={`flex`}>
+                            <Layout alignItems={"center"}>
+                                <div className={`flex`}>
+                                    <Link to={"/super-admin/dashboard/"} className={classes.logo}>
+                                        <img src={"/logo.png"} style={{height: 40}} alt={"logo"}/>
+                                    </Link>
+                                </div>
+                                <div>
+                                    <IconButton>
+                                        <BellOutlineIcon/>
+                                    </IconButton>
+                                    <IconButton onClick={(event) => {
+                                        this.setState({anchorEl: event.currentTarget})
+                                    }}>
+                                        <AccountIcon/>
+                                    </IconButton>
+                                    <Menu
+                                        anchorEl={this.state.anchorEl}
+                                        open={Boolean(this.state.anchorEl)}
+                                        onClose={() => {
+                                            this.setState({anchorEl: null})
+                                        }}
+                                    >
+                                        <MenuItem onClick={this.handleClose}>Logout</MenuItem>
+                                    </Menu>
+                                </div>
+                            </Layout>
+                            <Layout direction="column">
+                                <Tabs centered fullWidth value={currentTab}>
+                                    {
+                                        pages.map((page, index) => {
+                                            let PageIcon = Icon(page.icon);
+                                            let iconClass = (index == currentTab) ? classes.selected : '';
+                                            return <Tab classes={{label: classes.tabIcon}}
+                                                        label={<Layout alignItems={"center"}><PageIcon
+                                                            className={`${iconClass} ${this.props.classes.navIcon}`}/>{page.label}
+                                                        </Layout>}
+                                                        component={Link} to={page.url} key={index}>
+                                            </Tab>;
+                                        })
+                                    }
+                                </Tabs>
+                            </Layout>
+                        </Layout>
+                    </Toolbar>
+                </AppBar>
+                {this.props.children}
+            </Layout>
+        </div>;
+    }
 });
 
 export default connect(store => store)(Index);
--- frontend-client/src/super-admin/dashboard/vet-centers/index.js
+++ frontend-client/src/super-admin/dashboard/vet-centers/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../../../components/layout";
-import {Typography, TextField, List, ListItem} from "@material-ui/core/index";
+import {Typography, TextField,Button, List, ListItem} from "@material-ui/core/index";
 import {Link, Switch, Route} from "react-router-dom";
 import {withStyles} from "@material-ui/core/styles"
 import style from "../style";
@@ -8,6 +8,7 @@ import AddCenter from "./add-center"
 import Overview from "./overview"
 import VaccinationCenterManage from "./vaccination-center-manage"
 import {connect} from "react-redux"
+
 
 const sideNavPages = [
 	{label: "Add Vet Center", url: "/super-admin/dashboard/vet-centers/add-center"},
@@ -20,45 +21,62 @@ let Index = withStyles((theme) => {
 })(class extends React.Component {
 	componentWillMount() {
 		if (this.props.onPageChange) {
-			this.props.onPageChange("/super-admin/dashboard/vet-centers");
+			this.props.onPageChange("/super-admin/dashboard/pets");
 		}
 	}
 
 	render() {
 		const {classes} = this.props;
 		return <Layout direction={"column"} flex={1} className={classes.body}>
-			<Layout className={`container ${classes.flex}`}>
-				<Layout className={classes.leftSection}>
-					<Layout direction={"column"} className={classes.staticSection}>
-						<Typography variant="title" className={classes.title}>
-							Manage Vet Centers
-						</Typography>
-						<TextField className={classes.searchField} placeholder={"Search"}/>
-						<List>
-							{
-								sideNavPages.map((item, index) => {
-									return <Link key={index} to={item.url}>
-										<ListItem button> {item.label} </ListItem>
-									</Link>
-								})
-							}
-						</List>
+
+			<Layout className={`container ${classes.flex}`} direction={"column"}>
+
+				<Layout>
+
+                    <Typography style={{flex:1}}variant="title" className={classes.title}>
+                    Manage Vet Centers
+                    </Typography>
+
+					<TextField className={classes.searchField} placeholder={"Search"}/>
+					<Layout justifyContent={"flex-end"} className={classes.actions}>
+						<Button variant={"raised"} color={"primary"} type={"submit"}> + Add </Button>
 					</Layout>
 				</Layout>
-				<Layout direction={"column"} className={classes.rightSection}>
-					<Switch>
-						<Route exact path={"/super-admin/dashboard/vet-centers"} render={(props) => {
-							return <Overview location={props.location}/>;
-						}}/>
-						<Route path={"/super-admin/dashboard/vet-centers/add-center"} render={(props) => {
-							return <AddCenter location={props.location}/>;
-						}}/>
-						<Route exact path={"/super-admin/dashboard/vet-centers/:center_id/manage"} render={(props) => {
-							return <VaccinationCenterManage location={props.location}
-							                                centerId={props.match.params.center_id}/>;
-						}}/>
-					</Switch>
-				</Layout>
+
+				{/*<Layout className={classes.leftSection}>*/}
+					{/*<Layout direction={"column"} className={classes.staticSection}>*/}
+						{/*<Typography variant="title" className={classes.title}>*/}
+							{/*Manage Vet Centers*/}
+						{/*</Typography>*/}
+
+						{/*<List>*/}
+							{/*{*/}
+								{/*sideNavPages.map((item, index) => {*/}
+									{/*return <Link key={index} to={item.url}>*/}
+										{/*<ListItem button> {item.label} </ListItem>*/}
+									{/*</Link>*/}
+								{/*})*/}
+							{/*}*/}
+						{/*</List>*/}
+					{/*</Layout>*/}
+				{/*</Layout>*/}
+
+
+
+				{/*<Layout direction={"column"} className={classes.rightSection}>*/}
+					{/*<Switch>*/}
+						{/*<Route exact path={"/super-admin/dashboard/vet-centers"} render={(props) => {*/}
+							{/*return <Overview location={props.location}/>;*/}
+						{/*}}/>*/}
+						{/*<Route path={"/super-admin/dashboard/vet-centers/add-center"} render={(props) => {*/}
+							{/*return <AddCenter location={props.location}/>;*/}
+						{/*}}/>*/}
+						{/*<Route exact path={"/super-admin/dashboard/vet-centers/:center_id/manage"} render={(props) => {*/}
+							{/*return <VaccinationCenterManage location={props.location}*/}
+							                                {/*centerId={props.match.params.center_id}/>;*/}
+						{/*}}/>*/}
+					{/*</Switch>*/}
+				{/*</Layout>*/}
 			</Layout>
 		</Layout>;
 	}
