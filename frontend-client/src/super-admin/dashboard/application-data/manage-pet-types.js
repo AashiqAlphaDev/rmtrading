cdaff3c1e61b17bc75fe2e88367b3a4f6decc0be
Aashiq <aashiqalimohammed@gmail.com>
2018-06-22 06:13:42 +0530

    changes

--- frontend-client/src/super-admin/dashboard/application-data/manage-pet-types.js
+++ frontend-client/src/super-admin/dashboard/application-data/manage-pet-types.js
@@ -5,21 +5,10 @@ import style from "../style";
 import Layout from "../../../components/layout";
 import {Button, Paper, TextField} from "@material-ui/core/es/index";
 import InputContainer from "../../../components/input"
+import AnnotatedSection from "../../../components/annotated-section";
 
 
 
-let InputContainerTemplate = ({title})=>{
-	return	<div>
-	<InputContainer label={"Field Name " + title}>
-		<TextField/>
-	</InputContainer>
-	<InputContainer label={"Field Type " + title}>
-			<TextField/>
-		</InputContainer>
-
-	</div>
-
-}
 
 export default withStyles((theme) => {
 	return {
@@ -33,33 +22,40 @@ export default withStyles((theme) => {
         }
 	}
 })(class extends React.Component {
-    state = {
-        fields:[1]
-    };
+	state={
+		fieldName:"",
+		fieldType:""
+	}
+
 	render() {
 
 		const {classes} = this.props;
-		console.log(this.state)
-		return <Layout direction={"column"} flex={1} className={classes.body}>
-			<Layout className={`container ${classes.flex}`} direction={"column"}>
-				<Paper >
-				<Layout justifyContent={"center"} direction={"column"}>
 
-                    {
-                        this.state.fields.map((item, index) => {
-                            return <InputContainerTemplate key={index} title={index+1}/>
-                        })
-                    }
-					<Button onClick={() => {
-						var tempArray = this.state.fields;
-						tempArray.push(tempArray.length);
-						this.setState({fields:tempArray})
-						console.log(this.state)
-                    }}> + Add A field</Button>
+		return <AnnotatedSection
+			title={"Add Biometrics For Pet Type "}
+			desc={"Please provide the information to update Biometric Field for pets."}
+			backButton={{url: "/super-admin/dashboard/vet-centers"}}
+			className={classes.body}>
+			<Paper className={classes.paperPage}>
+				<Layout alignItems={"flex-end"}>
+			<InputContainer label={"Field Name"}>
+				<TextField onChange={(event) => {
+					this.setState({fieldName: event.target.value})
+				}}></TextField>
+			</InputContainer>
+			<InputContainer label={"Field Type"}>
+				<TextField onChange={(event) => {
+					this.setState({fieldType: event.target.value})
+				}}></TextField>
+			</InputContainer>
+
+					<Layout justifyContent={"flex-end"} >
+
+						<Button variant={"raised"} color={"primary"} style={{"padding-top":11,"padding-bottom":11}} type={"submit"}> Add </Button>
+					</Layout>
 
 				</Layout>
-				</Paper>
-		</Layout>
-		</Layout>
+			</Paper>
+		</AnnotatedSection>
 	}
 })
