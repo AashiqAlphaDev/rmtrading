a5b54180046c2f3a7ffa62fbdf4e6bb676503f76
Karthik Thirumalasetti <karthik@appsfly.io>
2018-06-24 16:31:13 +0530

    changes

--- frontend-client/src/admin/dashboard/pets/visit.js
+++ frontend-client/src/admin/dashboard/pets/visit.js
@@ -6,26 +6,28 @@ import {REQUEST_PET_FETCH} from "../../../stores/pets/actions";
 import {QUERY_VACCINATIONS} from "../../../stores/vaccinations/actions";
 import InputContainer from "../../../components/input"
 import {
-    Button, Dialog,
-    DialogActions,
-    DialogContent,
-    DialogContentText,
-    DialogTitle,
-    TextField,
-    Typography,
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableRow, Paper
+	Button, Dialog,
+	DialogActions,
+	DialogContent,
+	DialogContentText,
+	DialogTitle,
+	TextField,
+	Typography,
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableRow, Paper
 } from "@material-ui/core/es/index";
 import {REQUEST_PET_TYPE_FETCH} from "../../../stores/pet-types/actions";
 import moment from "moment"
-import {InformationIcon} from "mdi-react/dist/index.es";
+import Layout from "../../../components/layout";
+import {REQUEST_UPDATE_TOKEN} from "../../../stores/tokens/actions";
+import QrReader from 'react-qr-reader';
 
 
 var dates = {
-	convert:function(d) {
+	convert: function (d) {
 		// Converts the date in d to a date-object. The input can be:
 		//   a date object: returned without modification
 		//  an array      : Interpreted as [year,month,day]. NOTE: month is 0-11.
@@ -37,14 +39,14 @@ var dates = {
 		//                  attributes.  **NOTE** month is 0-11.
 		return (
 			d.constructor === Date ? d :
-				d.constructor === Array ? new Date(d[0],d[1],d[2]) :
+				d.constructor === Array ? new Date(d[0], d[1], d[2]) :
 					d.constructor === Number ? new Date(d) :
 						d.constructor === String ? new Date(d) :
-							typeof d === "object" ? new Date(d.year,d.month,d.date) :
+							typeof d === "object" ? new Date(d.year, d.month, d.date) :
 								NaN
 		);
 	},
-	compare:function(a,b) {
+	compare: function (a, b) {
 		// Compare two dates (could be of any type supported by the convert
 		// function above) and returns:
 		//  -1 : if a < b
@@ -53,18 +55,18 @@ var dates = {
 		// NaN : if a or b is an illegal date
 		// NOTE: The code inside isFinite does an assignment (=).
 		return (
-			isFinite(a=this.convert(a).valueOf()) &&
-			isFinite(b=this.convert(b).valueOf()) ?
-				(a>b)-(a<b) :
+			isFinite(a = this.convert(a).valueOf()) &&
+			isFinite(b = this.convert(b).valueOf()) ?
+				(a > b) - (a < b) :
 				NaN
 		);
 	},
-	inRange:function(d,start,end) {
-		console.log(d,start,end)
+	inRange: function (d, start, end) {
+		return true;
 		return (
-			isFinite(d=this.convert(d).valueOf()) &&
-			isFinite(start=this.convert(start).valueOf()) &&
-			isFinite(end=this.convert(end).valueOf()) ?
+			isFinite(d = this.convert(d).valueOf()) &&
+			isFinite(start = this.convert(start).valueOf()) &&
+			isFinite(end = this.convert(end).valueOf()) ?
 				start <= d && d <= end :
 				NaN
 		);
@@ -91,90 +93,133 @@ let Index = withStyles((theme) => {
 			marginBottom: theme.spacing.unit * 3,
 			padding: theme.spacing.unit * 1
 		},
-        paper: {
-            marginTop: theme.spacing.unit * 1,
-            display: "flex"
-        },
+		paper: {
+			marginTop: theme.spacing.unit * 1,
+			display: "flex"
+		},
 	}
 })(class extends React.Component {
-	state= {
-		openAddBiometrics:false,
-		data:{
-			height:0,
-			weight:0
+	state = {
+		openAddBiometrics: false,
+		data: {
+			height: 0,
+			weight: 0
 		},
-		sample:false
+		sample: false,
+		isBarCode: false
 	}
 
 	componentWillMount() {
-		this.props.dispatch({type:REQUEST_PET_FETCH, payload:{pet_id:this.props.match.params.pet_id}});
-		this.props.dispatch({type:QUERY_VACCINATIONS,payload:{pet_id:this.props.match.params.pet_id}});
-
+		this.props.dispatch({type: REQUEST_PET_FETCH, payload: {pet_id: this.props.match.params.pet_id}});
+		this.props.dispatch({type: QUERY_VACCINATIONS, payload: {pet_id: this.props.match.params.pet_id}});
 	}
 
-	componentWillReceiveProps(nextProps){
-		if(nextProps.petDetail.pet_type && !nextProps.petTypes.petTypeDetail){
-			this.props.dispatch({type:REQUEST_PET_TYPE_FETCH, payload:{pet_type_id:nextProps.petDetail.pet_type}})
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.petDetail.pet_type != nextProps.petTypes.petTypeDetail._id) {
+			this.props.dispatch({type: REQUEST_PET_TYPE_FETCH, payload: {pet_type_id: nextProps.petDetail.pet_type}})
 		}
 	}
 
 	render() {
 		const {classes} = this.props;
-		if(this.props.petTypes.petTypeDetail){
+		if (this.props.petTypes.petTypeDetail) {
 			return <div className={classes.body}>
-				<Button onClick={()=>{
-					this.setState({openAddBiometrics:true});
-				}}> Record Reading </Button>
+				<Paper className={`${classes.paperPage} ${classes.paper}`}>
+					<Button onClick={() => {
+						this.setState({openAddBiometrics: true});
+					}}> Record Reading </Button>
+				</Paper>
 
+				{
+					this.props.vaccinations.list.length > 0 &&
+					<Paper className={`${classes.paperPage} ${classes.paper}`}>
 
-
-                {
-                    this.props.vaccinations.list.length > 0 &&
-					<Paper className={`${classes.paperPage}`}>
-
-					<Table>
-						<TableHead>
-							<TableRow>
-								<TableCell>#</TableCell>
-								<TableCell>Vaccine Name</TableCell>
-								<TableCell>Dose</TableCell>
-								<TableCell>Start Date</TableCell>
-								<TableCell>Due Date</TableCell>
-							</TableRow>
-						</TableHead>
-						<TableBody>
-                            {
-                                this.props.vaccinations.list.map((item, index) => {
-                                	var inRange = dates.inRange(new Date(), new Date(item.catch_up_period.start), new Date(item.catch_up_period.due_date))
-                                    return <TableRow key={index} style={inRange?{}:{opacity:0.5}}>
-										<TableCell>{index + 1}</TableCell>
-										<TableCell>{item.data.vaccine}</TableCell>
-										<TableCell>{item.dose}</TableCell>
-										<TableCell>{moment(item.catch_up_period.start).format("MMMM Do YYYY")}</TableCell>
-										<TableCell>{moment(item.catch_up_period.due_date).format("MMMM Do YYYY")}</TableCell>
-										<TableCell><Button disabled={!inRange} onClick={()=>{
-											this.setState({currentVaccine:item});
-										}}>Vaccinate</Button></TableCell>
-									</TableRow>
-                                })
-                            }
-						</TableBody>
-					</Table>
+						<Table>
+							<TableHead>
+								<TableRow>
+									<TableCell>#</TableCell>
+									<TableCell>Vaccine Name</TableCell>
+									<TableCell>Dose</TableCell>
+									<TableCell>Start Date</TableCell>
+									<TableCell>Due Date</TableCell>
+								</TableRow>
+							</TableHead>
+							<TableBody>
+								{
+									this.props.vaccinations.list.map((item, index) => {
+										var inRange = dates.inRange(new Date(), new Date(item.catch_up_period.start), new Date(item.catch_up_period.due_date))
+										return <TableRow key={index} style={inRange ? {} : {opacity: 0.5}}>
+											<TableCell>{index + 1}</TableCell>
+											<TableCell>{item.data.vaccine}</TableCell>
+											<TableCell>{item.dose}</TableCell>
+											<TableCell>{moment(item.catch_up_period.start).format("MMMM Do YYYY")}</TableCell>
+											<TableCell>{moment(item.catch_up_period.due_date).format("MMMM Do YYYY")}</TableCell>
+											<TableCell><Button disabled={!inRange} onClick={() => {
+												this.setState({currentVaccine: item});
+											}}>Vaccinate</Button></TableCell>
+										</TableRow>
+									})
+								}
+							</TableBody>
+						</Table>
 					</Paper>
-                }
+				}
 
 				<Dialog
 					open={Boolean(this.state.currentVaccine)}
 					onClose={() => {
-						this.setState({currentVaccine:null});
+						this.setState({currentVaccine: null});
 					}}
 					aria-labelledby="form-dialog-title"
 				>
 					<form onSubmit={(event) => {
 						event.preventDefault();
 					}}>
-						<DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+						<DialogTitle id="form-dialog-title">Vaccination Detail</DialogTitle>
+						<DialogContent>
+							<Layout direction={"column"}>
+								<Button onClick={() => {
+									this.setState({isBarCode: !this.state.isBarCode})
+								}}>{this.state.isBarCode ? "QR Code" : "Bar Code"}</Button>
+								{
+									this.state.isBarCode &&
+									<div>
+										<Typography>Please enter barcode of vaccination vail</Typography>
+										<TextField autoFocus onChange={(e)=>{
+											this.setState({vaccination_code:e.target.value});
+										}}/>
+									</div>
+								}
 
+								{
+									!this.state.isBarCode &&
+									<div>
+										{
+											this.state.vaccination_code &&
+											<div>
+											<Typography>Vaccination Code:{this.state.vaccination_code}</Typography>
+												<Button onClick={()=>{this.setState({vaccination_code:null})}}>Scan Again</Button>
+											</div>
+										}
+										{
+											!this.state.vaccination_code &&
+											<QrReader
+												delay={this.state.delay}
+												onError={(err) => {
+													console.log(err);
+												}}
+												onScan={(result) => {
+													if (result) {
+														this.setState({vaccination_code: result});
+													}
+												}}
+												style={{width: 400, height: 400}}
+											/>
+										}
+									</div>
+								}
+							</Layout>
+						</DialogContent>
 					</form>
 				</Dialog>
 
@@ -196,12 +241,9 @@ let Index = withStyles((theme) => {
 									Kindly enter the details of the pet To be vaccinated.
 								</Typography>
 							</DialogContentText>
-
-
-
 							{
 								this.props.petTypes.petTypeDetail.vaccination_fields &&
-								this.props.petTypes.petTypeDetail.vaccination_fields.map((item)=>{
+								this.props.petTypes.petTypeDetail.vaccination_fields.map((item) => {
 									return <InputContainer label={item.name}>
 										<TextField
 											autoFocus
@@ -223,14 +265,14 @@ let Index = withStyles((theme) => {
 								Cancel
 							</Button>
 							<Button type="submit" color="primary">
-								Add
+								Save Reading
 							</Button>
 						</DialogActions>
 					</form>
 				</Dialog>
 			</div>
 		}
-		else{
+		else {
 			return <div></div>
 		}
 	}
